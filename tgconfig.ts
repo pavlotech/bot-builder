94b02bf538b7612e765c6cb78f8b3d7e82ac2b64
inoska <paveltarasenko799@gmail.com>
2024-05-21 11:26:16 +0200

    fixes

---
+++ tgconfig.ts
@@ -0,0 +1,13 @@
+// tgconfig.ts
+export default {
+  commands: [
+    {
+      command: 'start',
+      description: 'Start the bot and see the welcome message',
+    },
+    {
+      command: 'translate',
+      description: 'Translate text into a different language',
+    }
+  ]
+}
